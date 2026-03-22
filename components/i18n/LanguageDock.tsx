2fd373bd2901470908452d24d75362f91c93d148
TraderBross <nikokaya24@gmail.com>
2026-03-22 18:51:52 +0300

    fix(ui): hide language switcher on terminal route

--- components/i18n/LanguageDock.tsx
+++ components/i18n/LanguageDock.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import LanguageSwitcher from "@/components/i18n/LanguageSwitcher";
 
 export default function LanguageDock() {
+  const pathname = usePathname();
+  if (pathname?.startsWith("/terminal")) return null;
+
   return (
     <div className="fixed right-3 top-3 z-[70]">
       <LanguageSwitcher compact />
     </div>
   );
 }
-
